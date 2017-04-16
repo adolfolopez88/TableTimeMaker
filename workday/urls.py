6102623af2af93d1e5702a7d3f84d939ac9d383b
adolfo lopez <adolfolopez88@live.com>
2017-04-17 08:07:48 +1000

    add userprofiles
    login and singup

--- workday/urls.py
+++ workday/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.hello_world, name='hello'),
+    #url(r'^$', views.hello_world, name='hello'),
     url(r'^workday/(?P<pk>[0-9]+)/$', views.workday_detail, name='workday_detail'),
     url(r'^workday/new', views.new_workday, name='new_workday')
 ]
